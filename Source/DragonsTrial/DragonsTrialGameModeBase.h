8eae7544d96d79a3351372ad95c359f29d1739b4
Lee E <leeellermann@blueberrymountain.de>
2023-04-25 15:25:30 +0200

    c++ projekt aufgesetzt

---
+++ Source/DragonsTrial/DragonsTrialGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DragonsTrialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRAGONSTRIAL_API ADragonsTrialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
